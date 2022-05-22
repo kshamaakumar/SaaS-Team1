f83124e90f2707cba18f48e0e81334eb5fe1d936
ZiQi(Elbie) Li <81213490+ElbieLi@users.noreply.github.com>
2022-05-22 14:11:41 -0700

    Made search component a defult

--- Recipe-Website-Ang/src/app/app-routing.module.ts
+++ Recipe-Website-Ang/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RecipeComponent } from './recipe/recipe.component';
+import { SearchComponent } from './search/search.component';
 
 const routes: Routes = [
   {path: 'recipe', component:  RecipeComponent},
+  { path: '', component:  SearchComponent},
 ];
 
 @NgModule({
